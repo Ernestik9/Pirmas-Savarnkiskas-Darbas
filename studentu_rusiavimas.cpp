acd75549a2e9d4d726aa3d60a4406dc362b21043
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-10-14 11:21:27 +0300

    Update studentu_rusiavimas.cpp

--- studentu_rusiavimas.cpp
+++ studentu_rusiavimas.cpp
@@ -2,20 +2,20 @@
 #include <fstream>
 
 double studentas::galutinisBalas() {
-    double pazymiuVidurkis = std::accumulate(paz.begin(), paz.end(), 0.0) / paz.size();
+    double pazymiuVidurkis = accumulate(paz.begin(), paz.end(), 0.0) / paz.size();
     return 0.4 * pazymiuVidurkis + 0.6 * egz;
 }
 
-void rusiuotiStudentus(const vector<studentas>& grupe, const std::string& vargsiukaiFailas, const std::string& kietiakiaiFailas) {
-    std::ofstream fout_vargsiukai(vargsiukaiFailas);
-    std::ofstream fout_kietiakiai(kietiakiaiFailas);
+void rusiuotiStudentus(const vector<studentas>& grupe, const string& vargsiukaiFailas, const string& kietiakiaiFailas) {
+    ofstream fout_vargsiukai(vargsiukaiFailas);
+    ofstream fout_kietiakiai(kietiakiaiFailas);
 
     for (const auto& s : grupe) {
         double galutinis = s.galutinisBalas();
         if (galutinis < 5.0) {
-            fout_vargsiukai << s.vard_() << " " << s.pav_() << " " << galutinis << std::endl;
+            fout_vargsiukai << s.vard_() << " " << s.pav_() << " " << galutinis << endl;
         } else {
-            fout_kietiakiai << s.vard_() << " " << s.pav_() << " " << galutinis << std::endl;
+            fout_kietiakiai << s.vard_() << " " << s.pav_() << " " << galutinis << endl;
         }
     }
     fout_vargsiukai.close();
