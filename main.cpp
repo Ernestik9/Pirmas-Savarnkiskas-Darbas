2c058d4ad3cfee565db442841d6811921f0b351b
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-10-14 11:32:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@
 
 int main() {
    
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
     
     generuotiStudentus(1000, "studentai1000.txt");
     
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
     cout << "Failų generavimo laikas: " << duration.count() << " ms" << endl;
 
 
-    start = std::chrono::high_resolution_clock::now();
+    start = chrono::high_resolution_clock::now();
 
     rusiuotiStudentus("studentai1000.txt", "vargsiukai.txt", "kietiakiai.txt");
 
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    end = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::milliseconds>(end - start);
     cout << "Rūšiavimo ir įrašymo į failus laikas: " << duration.count() << " ms" << endl;
 
     return 0;
