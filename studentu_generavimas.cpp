62d09fefd4ebcccce4dcde1f33355fb93d0feca6
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-10-14 11:19:46 +0300

    Update studentu_generavimas.cpp

--- studentu_generavimas.cpp
+++ studentu_generavimas.cpp
@@ -2,24 +2,24 @@
 #include <fstream>
 #include <random>
 
-void generuotiStudentus(int kiekis, const std::string& failoPavadinimas) {
-    std::ofstream fout(failoPavadinimas);
+void generuotiStudentus(int kiekis, const string& failoPavadinimas) {
+    ofstream fout(failoPavadinimas);
     if (!fout) {
-        std::cerr << "Nepavyko sukurti failo!" << std::endl;
+        cout << "Nepavyko sukurti failo!" << endl;
         return;
     }
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> pazymiai(1, 10);
-    std::uniform_int_distribution<> egzaminas(1, 10);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> pazymiai(1, 10);
+    uniform_int_distribution<> egzaminas(1, 10);
 
     for (int i = 1; i <= kiekis; ++i) {
         fout << "Vardas" << i << " Pavarde" << i << " ";
         for (int j = 0; j < 5; ++j) {
             fout << pazymiai(gen) << " ";
         }
-        fout << egzaminas(gen) << std::endl;
+        fout << egzaminas(gen) << endl;
     }
     fout.close();
 }
