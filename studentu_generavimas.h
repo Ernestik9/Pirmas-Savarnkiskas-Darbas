46c31ee0756a27c761ac4edb8c56e1a24a2292ff
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-10-15 19:45:55 +0300

    Update studentu_generavimas.h

--- studentu_generavimas.h
+++ studentu_generavimas.h
@@ -1,25 +1,3 @@
-#include "myLib.h"
-#include <fstream>
-#include <random>
+#include "mylib.h"
 
-void generuotiStudentus(int kiekis, const string& failoPavadinimas) {
-    ofstream fout(failoPavadinimas);
-    if (!fout) {
-        cout << "Nepavyko sukurti failo!" << endl;
-        return;
-    }
-
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> pazymiai(1, 10);
-    uniform_int_distribution<> egzaminas(1, 10);
-
-    for (int i = 1; i <= kiekis; ++i) {
-        fout << "Vardas" << i << " Pavarde" << i << " ";
-        for (int j = 0; j < 5; ++j) {
-            fout << pazymiai(gen) << " ";
-        }
-        fout << egzaminas(gen) << endl;
-    }
-    fout.close();
-}
+void generuotiStudentus(int kiekis, const std::string& failoPavadinimas);
