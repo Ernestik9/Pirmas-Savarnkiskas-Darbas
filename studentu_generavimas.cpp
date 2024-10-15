9e34f5031f8a5fcb4d85212583214b19556d89b5
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-10-15 19:54:51 +0300

    Create studentu_generavimas.cpp

---
+++ studentu_generavimas.cpp
@@ -0,0 +1,23 @@
+#include "studentu_generavimas.h"
+
+void generuotiStudentus(int kiekis, const std::string& failoPavadinimas) {
+    std::ofstream fout(failoPavadinimas);
+    if (!fout) {
+        std::cout << "Nepavyko sukurti failo!" << std::endl;
+        return;
+    }
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> pazymiai(1, 10);
+    std::uniform_int_distribution<> egzaminas(1, 10);
+
+    for (int i = 1; i <= kiekis; ++i) {
+        fout << "Vardas" << i << " Pavarde" << i << " ";
+        for (int j = 0; j < 5; ++j) {
+            fout << pazymiai(gen) << " ";
+        }
+        fout << egzaminas(gen) << std::endl;
+    }
+    fout.close();
+}
