b3e7c4b004c8beffd0a592856f15cc94214d02d3
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-10 21:46:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "studentu_generavimas.h"
 #include "studentu_rusiavimas.h"
 
+
 int main() {
     int kiekiai[] = {1000, 10000, 100000, 1000000, 10000000};
     std::string failuPavadinimai[] = {
@@ -14,9 +15,31 @@ int main() {
     };
 
     for (int i = 0; i < 5; ++i) {
-        std::cout << "Rusiuoti studentus std::vector:\n";
-        rusiuotiStudentus1<std::vector<studentas>>(failuPavadinimai[i], "vargsiukai_vector.txt", "kietiakiai_vector.txt");
+        std::cout << "Apdorojama failą: " << failuPavadinimai[i] << " (" << kiekiai[i] << " studentų)" << std::endl;
 
+
+        std::cout << "Strategija 1 su std::vector:\n";
+        rusiuotiStudentusStrategija1<std::vector<studentas>>(failuPavadinimai[i], "vargsiukai_vector1.txt", "kietiakiai_vector1.txt");
+
+
+        std::cout << "Strategija 1 su std::list:\n";
+        rusiuotiStudentusStrategija1<std::list<studentas>>(failuPavadinimai[i], "vargsiukai_list1.txt", "kietiakiai_list1.txt");
+
+        std::cout << "Strategija 1 su std::deque:\n";
+        rusiuotiStudentusStrategija1<std::deque<studentas>>(failuPavadinimai[i], "vargsiukai_deque1.txt", "kietiakiai_deque1.txt");
+
+        std::cout << "Strategija 2 su std::vector:\n";
+        rusiuotiStudentusStrategija2<std::vector<studentas>>(failuPavadinimai[i], "vargsiukai_vector2.txt");
+
+ 
+        std::cout << "Strategija 2 su std::list:\n";
+        rusiuotiStudentusStrategija2<std::list<studentas>>(failuPavadinimai[i], "vargsiukai_list2.txt");
+
+
+        std::cout << "Strategija 2 su std::deque:\n";
+        rusiuotiStudentusStrategija2<std::deque<studentas>>(failuPavadinimai[i], "vargsiukai_deque2.txt");
+
+        std::cout << "-------------------------------------------------------\n";
     }
 
     return 0;
