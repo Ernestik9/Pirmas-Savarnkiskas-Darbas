25ca428c77714105be442e26992a6ad1db8f7f56
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-10 21:44:24 +0200

    Update studentu_rusiavimas.h

--- studentu_rusiavimas.h
+++ studentu_rusiavimas.h
@@ -2,11 +2,26 @@
 #define STUDENTU_RUSIAVIMAS_H_INCLUDED
 #include "mylib.h"
 
+
+struct studentas {
+    std::string vardas, pavarde;
+    std::vector<int> paz;
+    int egz;
+
+    double galutinisBalas() const {
+        double pazymiuVidurkis = std::accumulate(paz.begin(), paz.end(), 0.0) / paz.size();
+        return 0.4 * pazymiuVidurkis + 0.6 * egz;
+    }
+
+    std::string vard_() const { return vardas; }
+    std::string pav_() const { return pavarde; }
+};
+
 template <typename Container>
-void rusiuotiStudentus1(const std::string& inputFailas, const std::string& vargsiukaiFailas, const std::string& kietiakiaiFailas) {
+void rusiuotiStudentusStrategija1(const std::string& inputFailas, const std::string& vargsiukaiFailas, const std::string& kietiakiaiFailas) {
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Nuskaitymas
+
     std::ifstream fin(inputFailas);
     if (!fin) {
         std::cout << "Nepavyko atidaryti failo!" << std::endl;
@@ -25,33 +40,95 @@ void rusiuotiStudentus1(const std::string& inputFailas, const std::string& vargs
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Duomenu nuskaitymas uztruko: " << duration.count() << " ms" << std::endl;
+    std::cout << "Duomenų nuskaitymas užtruko: " << duration.count() << " ms" << std::endl;
 
-    // Rūšiavimas į du konteinerius
+
     start = std::chrono::high_resolution_clock::now();
     Container vargsiukai, kietiakiai;
+    for (const auto& stud : grupe) {
+        double galutinis = stud.galutinisBalas();
+        if (galutinis < 5.0) {
+            vargsiukai.push_back(stud);
+        } else {
+            kietiakiai.push_back(stud);
+        }
+    }
 
-    std::partition_copy(grupe.begin(), grupe.end(), std::back_inserter(vargsiukai), std::back_inserter(kietiakiai),
-                        [](const studentas& stud) { return stud.galutinisBalas() < 5.0; });
-
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Rūsiavimas uztruko: " << duration.count() << " ms" << std::endl;
-
-    // Įrašymas į failus
     std::ofstream fout_vargsiukai(vargsiukaiFailas);
     std::ofstream fout_kietiakiai(kietiakiaiFailas);
+    if (!fout_vargsiukai || !fout_kietiakiai) {
+        std::cout << "Nepavyko atidaryti failų!" << std::endl;
+        return;
+    }
 
     for (const auto& stud : vargsiukai) {
         fout_vargsiukai << stud.vard_() << " " << stud.pav_() << " " << stud.galutinisBalas() << std::endl;
     }
+
     for (const auto& stud : kietiakiai) {
         fout_kietiakiai << stud.vard_() << " " << stud.pav_() << " " << stud.galutinisBalas() << std::endl;
     }
 
     fout_vargsiukai.close();
     fout_kietiakiai.close();
+
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Rūšiavimas ir išvedimas užtruko: " << duration.count() << " ms" << std::endl;
 }
 
+
+template <typename Container>
+void rusiuotiStudentusStrategija2(const std::string& inputFailas, const std::string& vargsiukaiFailas) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+ 
+    std::ifstream fin(inputFailas);
+    if (!fin) {
+        std::cout << "Nepavyko atidaryti failo!" << std::endl;
+        return;
+    }
+
+    Container grupe;
+    studentas s;
+    while (fin >> s.vardas >> s.pavarde) {
+        s.paz.resize(5);
+        for (int& paz : s.paz) fin >> paz;
+        fin >> s.egz;
+        grupe.push_back(s);
+    }
+    fin.close();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Duomenų nuskaitymas užtruko: " << duration.count() << " ms" << std::endl;
+
+
+    start = std::chrono::high_resolution_clock::now();
+    Container vargsiukai;
+    grupe.erase(std::remove_if(grupe.begin(), grupe.end(), [&vargsiukai](const studentas& stud) {
+        if (stud.galutinisBalas() < 5.0) {
+            vargsiukai.push_back(stud);
+            return true;
+        }
+        return false;
+    }), grupe.end());
+
+    std::ofstream fout_vargsiukai(vargsiukaiFailas);
+    if (!fout_vargsiukai) {
+        std::cout << "Nepavyko atidaryti failo!" << std::endl;
+        return;
+    }
+
+    for (const auto& stud : vargsiukai) {
+        fout_vargsiukai << stud.vard_() << " " << stud.pav_() << " " << stud.galutinisBalas() << std::endl;
+    }
+
+    fout_vargsiukai.close();
+
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Rūšiavimas ir išvedimas užtruko: " << duration.count() << " ms" << std::endl;
+}
 
 #endif // STUDENTU_RUSIAVIMAS_H_INCLUDED
