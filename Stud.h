cd09d9c25db9212b96a16162f5a0695937ba2142
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-11 11:16:22 +0200

    Update Stud.h

--- Stud.h
+++ Stud.h
@@ -1,58 +1,22 @@
-##include studentu_rusiavimas.h"
-#include "mylib.h"
+#ifndef STUD_H_INCLUDED
+#define STUD_H_INCLUDED
+#include "MyLib.h"
 
-double studentas::galutinisBalas() const {  
-    double pazymiuVidurkis = std::accumulate(paz.begin(), paz.end(), 0.0) / paz.size();
-    return 0.4 * pazymiuVidurkis + 0.6 * egz;
-}
+struct Stud {
+    std::string vardas, pavarde;
+    std::vector<double> ND;
+    double egz, rez;
+};
 
-void rusiuotiStudentus(const std::string& inputFailas, const std::string& vargsiukaiFailas, const std::string& kietiakiaiFailas) {
+void ived(Stud &Lok);
+void output(const Stud &Lok, bool naudotiMediana);
+void val(Stud &Lok);
+void skaiciuotiGalutini(Stud &Lok, bool naudotiMediana);
+double skaiciuotiVidurki(const std::vector<double>& ND);
+double skaiciuotiMediana(const std::vector<double>& ND);
+void generuotiBalus(Stud &Lok, bool atsitiktiniai = false);
+void nuskaitytiIrasusIsFailo(const std::string& failas, std::vector<Stud>& Vec1);
 
-    auto start = std::chrono::high_resolution_clock::now();
 
+#endif // STUD_H_INCLUDED
 
-    std::ifstream fin(inputFailas);
-    if (!fin) {
-        std::cout << "Nepavyko atidaryti failo!" << std::endl;
-        return;
-    }
-
-    std::vector<studentas> grupe;
-    studentas s;
-    while (fin >> s.vardas >> s.pavarde) {
-        s.paz.resize(5);
-        for (int& paz : s.paz) fin >> paz;
-        fin >> s.egz;
-        grupe.push_back(s);
-    }
-    fin.close();
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Duomenu nuskaitymas uztruko: " << duration.count() << " ms" << std::endl;
-
-    // Rūšiavimas
-    start = std::chrono::high_resolution_clock::now();
-    std::ofstream fout_vargsiukai(vargsiukaiFailas);
-    std::ofstream fout_kietiakiai(kietiakiaiFailas);
-    if (!fout_vargsiukai || !fout_kietiakiai) {
-        std::cout << "Nepavyko atidaryti failu!" << std::endl;
-        return;
-    }
-
-    for (const auto& stud : grupe) {
-        double galutinis = stud.galutinisBalas();
-        if (galutinis < 5.0) {
-            fout_vargsiukai << stud.vard_() << " " << stud.pav_() << " " << galutinis << std::endl;
-        } else {
-            fout_kietiakiai << stud.vard_() << " " << stud.pav_() << " " << galutinis << std::endl;
-        }
-    }
-
-    fout_vargsiukai.close();
-    fout_kietiakiai.close();
-
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Rūsiavimas ir isvedimas i failus uztruko: " << duration.count() << " ms" << std::endl;
-}
