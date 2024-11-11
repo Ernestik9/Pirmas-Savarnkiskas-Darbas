6ffbb25c6a8cad00183f64dc1ad2e1b57413dea4
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-11 11:39:13 +0200

    Create Stud.h

---
+++ Stud.h
@@ -0,0 +1,15 @@
+#ifndef STUD_H_INCLUDED
+#define STUD_H_INCLUDED
+#include "MyLib.h"
+struct Stud {
+    std::string vardas, pavarde;
+    std::vector<double> ND;
+    double egz, rez;
+};
+void ived(Stud &Lok);
+void output(Stud &Lok);
+void val(Stud &Lok);
+void skaiciuotiGalutini(Stud &Lok, bool naudotiMediana);
+double skaiciuotiVidurki(const std::vector<double>& ND);
+double skaiciuotiMediana(const std::vector<double>& ND);
+#endif // STUD_H_INCLUDED
