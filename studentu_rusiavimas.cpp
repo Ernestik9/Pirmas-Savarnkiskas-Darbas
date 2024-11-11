29783735513fe60256100f0f081dccd9c45499a1
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-11 11:40:27 +0200

    Delete studentu_rusiavimas.cpp

--- studentu_rusiavimas.cpp
+++
@@ -1,9 +0,0 @@
-#include "studentu_rusiavimas.h"
-
-double studentas::galutinisBalas() const {
-    double pazymiuVidurkis = std::accumulate(paz.begin(), paz.end(), 0.0) / paz.size();
-    return 0.4 * pazymiuVidurkis + 0.6 * egz;
-}
-
-std::string studentas::vard_() const { return vardas; }
-std::string studentas::pav_() const { return pavarde; }
