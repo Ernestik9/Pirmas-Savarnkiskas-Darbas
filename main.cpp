4a96b360a7cd3eea744a19873b1d00dc93695a33
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-11 11:19:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,57 @@
-#include "mylib.h"
-#include <iostream>
-#include "studentu_generavimas.h"
-#include "studentu_rusiavimas.h"
+#include "Stud.h"
+#include "MyLib.h"
+
 
 int main() {
-    int kiekiai[] = {1000, 10000, 100000, 1000000, 10000000};
-    std::string failuPavadinimai[] = {
-        "studentai1000.txt",
-        "studentai10000.txt",
-        "studentai100000.txt",
-        "studentai1000000.txt",
-        "studentai10000000.txt"
-    };
+    srand(time(0));
 
-    for (int i = 0; i < 5; ++i) {
-        auto start = std::chrono::high_resolution_clock::now();
+    vector<Stud> Vec1;
+    Stud Temp;
 
 
-        generuotiStudentus(kiekiai[i], failuPavadinimai[i]);
+    cout << "Ar norite ivesti studentu duomenis (I) ar nuskaityti is failo (F)? ";
+    char pasirinkimas;
+    cin >> pasirinkimas;
 
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "Failas " << failuPavadinimai[i] << " sugeneruotas per: " << duration.count() << " ms" << std::endl;
-
-
-        std::cout << "Testavimas su std::vector:\n";
-        rusiuotiStudentus<std::vector<studentas>>(failuPavadinimai[i], "vargsiukai_vector.txt", "kietiakiai_vector.txt");
-
-
-        std::cout << "Testavimas su std::list:\n";
-        rusiuotiStudentus<std::list<studentas>>(failuPavadinimai[i], "vargsiukai_list.txt", "kietiakiai_list.txt");
-
-
-        std::cout << "Testavimas su std::deque:\n";
-        rusiuotiStudentus<std::deque<studentas>>(failuPavadinimai[i], "vargsiukai_deque.txt", "kietiakiai_deque.txt");
+    if (pasirinkimas == 'I' || pasirinkimas == 'i') {
+        cout << "Kiek studentu yra? ";
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cout << "Iveskite studento duomenis: " << endl;
+            ived(Temp);
+            Vec1.push_back(Temp);
+            val(Temp);
+        }
+    } else if (pasirinkimas == 'F' || pasirinkimas == 'f') {
+        try {
+            nuskaitytiIrasusIsFailo("kursiokai.txt", Vec1);
+        } catch (std::exception& e) {
+            cout << "Klaida: " << e.what() << endl;
+            return 1;
+        }
     }
+
+    char pasirinkimasGalutinio;
+    cout << "Skaiciuoti pagal mediana (m) ar vidurki (v)? ";
+    cin >> pasirinkimasGalutinio;
+
+    bool naudotiMediana = (pasirinkimasGalutinio == 'm');
+    cout << setw(15) << left << "Pavarde"
+         << setw(15) << left << "Vardas"
+         << setw(20) << left << (naudotiMediana ? "Galutinis (Med.)" : "Galutinis (Vid.)")
+         << setw(20) << left << "Galutinis (Vid.)" << endl;
+    cout << "----------------------------------------------------------" << endl;
+
+
+    sort(Vec1.begin(), Vec1.end(), [](const Stud& a, const Stud& b) {
+        return a.pavarde < b.pavarde;
+    });
+
+    for (int i = 0; i < Vec1.size(); i++) {
+    skaiciuotiGalutini(Vec1.at(i), naudotiMediana);
+    output(Vec1.at(i), naudotiMediana);
+}
 
     return 0;
 }
