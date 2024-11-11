f8d875f601bbeaf191473078b159d6036f326ad5
Ernestik9 <ernestas.prokopovic@gmail.com>
2024-11-11 11:18:20 +0200

    Update Stud.cpp

--- Stud.cpp
+++ Stud.cpp
@@ -1,5 +1,6 @@
 #include "Stud.h"
-#include <iomanip>
+#include "MyLib.h"
+
 
 void ived(Stud &Lok) {
     cout << "Iveskite Varda, Pavarde ir Egzamino ivertinima: ";
@@ -14,11 +15,14 @@ void ived(Stud &Lok) {
     }
 }
 
-void output(Stud &Lok) {
+void output(const Stud &Lok, bool naudotiMediana) {
     cout << setw(15) << left << Lok.pavarde
          << setw(15) << left << Lok.vardas
+         << setw(20) << left << setprecision(2) << std::fixed
+         << (naudotiMediana ? skaiciuotiMediana(Lok.ND) : skaiciuotiVidurki(Lok.ND)) // Pateikiamas rezultatas pagal medianą arba vidurkį
          << setw(20) << left << setprecision(2) << std::fixed << Lok.rez << endl;
 }
+
 
 void val(Stud &Lok) {
     Lok.vardas.clear();
@@ -46,5 +50,38 @@ void skaiciuotiGalutini(Stud &Lok, bool naudotiMediana) {
         Lok.rez = 0.4 * skaiciuotiMediana(Lok.ND) + 0.6 * Lok.egz;
     } else {
         Lok.rez = 0.4 * skaiciuotiVidurki(Lok.ND) + 0.6 * Lok.egz;
+    }
+}
+
+void generuotiBalus(Stud &Lok, bool atsitiktiniai) {
+    if (atsitiktiniai) {
+
+        Lok.egz = rand() % 11;
+        int ndCount = rand() % 6 + 1;
+        Lok.ND.clear();
+        for (int i = 0; i < ndCount; ++i) {
+            Lok.ND.push_back(rand() % 11);
+        }
+    }
+}
+
+void nuskaitytiIrasusIsFailo(const std::string& failas, std::vector<Stud>& Vec1) {
+    std::ifstream file(failas);
+    if (!file) {
+        throw std::runtime_error("Nepavyko atidaryti failo.");
+    }
+
+    Stud Temp;
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        ss >> Temp.pavarde >> Temp.vardas >> Temp.egz;
+
+        double pazymys;
+        while (ss >> pazymys) {
+            Temp.ND.push_back(pazymys);
+        }
+
+        Vec1.push_back(Temp);
     }
 }
